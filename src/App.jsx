92f660b763ebff1b246d9a6c261bb403579820e7
Nour Eldin <noureldin2662002@gmail.com>
2023-10-31 17:37:14 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,30 @@ import React from 'react';
 import './App.css';
 
 
-import firebase from "./firebase/app";
-import "./firebase/firestore";
-import "./firebase/auth";
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 
 import {useAuthstate} from 'react-firebase-hooks/auth';
 import{useCollectionData} from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyB9cQ_5Dp63S5w5lzNknCWmEK4zpTH3rtY",
   authDomain: "proj1-e9978.firebaseapp.com",
   projectId: "proj1-e9978",
   storageBucket: "proj1-e9978.appspot.com",
   messagingSenderId: "960018682541",
   appId: "1:960018682541:web:faf15960decbd0c373ac24"
+}
 
-})
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+const app = initializeApp(firebaseConfig);
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      hello
     </div>
   );
 }
