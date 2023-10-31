8c04b3eb5db95838167ba1c73139b0780f3179e8
PassantYasser <passantabutalib@gmail.com>
2023-10-31 17:13:26 +0200

    test firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+import firebase from "./firebase/app";
+import "./firebase/firestore";
+import "./firebase/auth";
+
+
+import {useAuthstate} from 'react-firebase-hooks/auth';
+import{useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB9cQ_5Dp63S5w5lzNknCWmEK4zpTH3rtY",
+  authDomain: "proj1-e9978.firebaseapp.com",
+  projectId: "proj1-e9978",
+  storageBucket: "proj1-e9978.appspot.com",
+  messagingSenderId: "960018682541",
+  appId: "1:960018682541:web:faf15960decbd0c373ac24"
+
+})
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
